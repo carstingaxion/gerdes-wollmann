70cfcdf628d0f32499c6055a95ba0665389c7a96
Carsten Bach <mail@carsten-bach.de>
2026-03-17 13:36:56 +0100

    Update color scheme

--- wp-content/themes/gerdes-wollmann/patterns/footer.php
+++ wp-content/themes/gerdes-wollmann/patterns/footer.php
@@ -21,23 +21,28 @@
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"width":"30%"} -->
-<div class="wp-block-column" style="flex-basis:30%"><!-- wp:heading {"level":4,"style":{"typography":{"letterSpacing":"0.16em","textTransform":"uppercase","fontWeight":"500"}},"textColor":"accent-1","fontSize":"small"} -->
+<!-- wp:column {"verticalAlignment":"bottom","width":"30%"} -->
+<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:30%"><!-- wp:heading {"level":4,"style":{"typography":{"letterSpacing":"0.16em","textTransform":"uppercase","fontWeight":"500"}},"textColor":"accent-1","fontSize":"small"} -->
 <h4 class="wp-block-heading has-accent-1-color has-text-color has-small-font-size" style="font-weight:500;letter-spacing:0.16em;text-transform:uppercase"><?php esc_html_e('Kontakt', 'gerdes-wollmann');?></h4>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.8"},"spacing":{"margin":{"top":"1rem"}}},"textColor":"accent-4","fontSize":"medium"} -->
-<p class="has-accent-4-color has-text-color has-medium-font-size" style="margin-top:1rem;line-height:1.8"><?php /* Translators: 1. is a 'br' HTML element, 2. is the start of a 'a' HTML element, 3. is the end of a 'a' HTML element, 4. is a 'br' HTML element, 5. is the start of a 'a' HTML element, 6. is the end of a 'a' HTML element */ 
-echo sprintf( esc_html__( 'Ludwig-Wucherer-Str. 10, 06108 Halle (Saale)%1$sTelefon: %2$s0345 678 23 33%3$s%4$s%5$spraxis@gerdes-wollmann.de%6$s', 'gerdes-wollmann' ), '<br>', '<a href="' . esc_url( 'tel:03456782333' ) . '">', '</a>', '<br>', '<a href="' . esc_url( 'mailto:praxis@gerdes-wollmann.de' ) . '">', '</a>' ); ?></p>
+<p class="has-accent-4-color has-text-color has-medium-font-size" style="margin-top:1rem;line-height:1.8"><?php /* Translators: 1. is a 'br' HTML element */ 
+echo sprintf( esc_html__( 'Ludwig-Wucherer-Str. 10,%1$s06108 Halle (Saale)', 'gerdes-wollmann' ), '<br>' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.8"},"spacing":{"margin":{"top":"1rem"}}},"textColor":"accent-4","fontSize":"medium"} -->
+<p class="has-accent-4-color has-text-color has-medium-font-size" style="margin-top:1rem;line-height:1.8"><?php /* Translators: 1. is the start of a 'a' HTML element, 2. is the end of a 'a' HTML element, 3. is a 'br' HTML element, 4. is the start of a 'a' HTML element, 5. is the end of a 'a' HTML element */ 
+echo sprintf( esc_html__( '%1$s0345 678 23 33%2$s%3$s%4$spraxis@gerdes-wollmann.de%5$s', 'gerdes-wollmann' ), '<a href="' . esc_url( 'tel:03456782333' ) . '">', '</a>', '<br>', '<a href="' . esc_url( 'mailto:praxis@gerdes-wollmann.de' ) . '">', '</a>' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"bottom","width":"30%"} -->
-<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:30%"><!-- wp:heading {"level":4,"style":{"typography":{"letterSpacing":"0.16em","textTransform":"uppercase","fontWeight":"500"}},"fontSize":"small"} -->
-<h4 class="wp-block-heading has-small-font-size" style="font-weight:500;letter-spacing:0.16em;text-transform:uppercase"><?php esc_html_e('Heute', 'gerdes-wollmann');?></h4>
+<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:30%"><!-- wp:heading {"level":4,"style":{"typography":{"letterSpacing":"0.16em","textTransform":"uppercase","fontWeight":"500"},"elements":{"link":{"color":{"text":"var:preset|color|accent-1"}}}},"textColor":"accent-1","fontSize":"small"} -->
+<h4 class="wp-block-heading has-accent-1-color has-text-color has-link-color has-small-font-size" style="font-weight:500;letter-spacing:0.16em;text-transform:uppercase"><?php esc_html_e('Heute', 'gerdes-wollmann');?></h4>
 <!-- /wp:heading -->
 
-<!-- wp:telex/block-telex-hours-block {"displayMode":"day","showReasonClosed":false,"friendlyTwelves":false} /--></div>
+<!-- wp:telex/block-telex-hours-block {"displayMode":"day","showReasonClosed":false,"friendlyTwelves":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2"} /--></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 
@@ -45,8 +50,5 @@ echo sprintf( esc_html__( 'Ludwig-Wucherer-Str. 10, 06108 Halle (Saale)%1$sTelef
 <hr class="wp-block-separator has-text-color has-contrast-2-color has-alpha-channel-opacity has-contrast-2-background-color has-background is-style-wide" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--30)"/>
 <!-- /wp:separator -->
 
-<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.5"}},"textColor":"contrast-2","fontSize":"small"} -->
-<p class="has-text-align-center has-contrast-2-color has-text-color has-small-font-size" style="line-height:1.5"><?php /* Translators: 1. is the start of a 'a' HTML element, 2. is the end of a 'a' HTML element */ 
-echo sprintf( esc_html__( 'Created with and powered by %1$sWordPress%2$s', 'gerdes-wollmann' ), '<a href="' . esc_url( 'https://wordpress.org' ) . '" target="_blank" rel="noopener noreferrer">', '</a>' ); ?></p>
-<!-- /wp:paragraph --></div>
+<!-- wp:navigation {"textColor":"base","backgroundColor":"contrast","overlayMenu":"never","layout":{"type":"flex","justifyContent":"center"}} /--></div>
 <!-- /wp:group -->
